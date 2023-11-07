78fbbf6903e830b601ca629a05a4d6a08fe15198
LucasBelusso1 <belussolucas@gmail.com>
2023-11-06 22:15:42 -0300

    feat: creates list orders handler and it's route

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -46,7 +46,8 @@ func main() {
 
 	webserver := webserver.NewWebServer(configs.WebServerPort)
 	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
-	webserver.AddHandler("/order", webOrderHandler.Create)
+	webserver.AddHandler(webserver.NewHttpHandler("post", "/order", webOrderHandler.Create))
+	webserver.AddHandler(webserver.NewHttpHandler("get", "/orders", webOrderHandler.List))
 	fmt.Println("Starting web server on port", configs.WebServerPort)
 	go webserver.Start()
 
